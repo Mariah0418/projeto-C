c8e9a6e7b62faf0e586563a8c6817d9a0ee15f36
Mariah0418 <mariah4418@gmail.com>
2025-05-21 23:21:26 -0400

    git commit -m "feat: adiciona estrutura inicial do projeto"

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
-int main(int argc, char const *argv[])
-{
-    printf ("ola mundo!");
+#include <stdio.h>
+
+int main() {
+    int a, b, soma;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+
+    soma = a + b;
+
+    printf("A soma de %d e %d é %d\n", a, b, soma);
+
     return 0;
 }
